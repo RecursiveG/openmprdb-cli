dc59893b2b1fdbb66ac34cedcbe634675fb1cb4e
Recursive G <g.recursive@gmail.com>
2021-08-11 00:24:48 -0500

    PUT /v1/submit/new

--- src/openmprdb-cli.cpp
+++ src/openmprdb-cli.cpp
@@ -1,4 +1,5 @@
 #include "gpgme.h"
+#include <chrono>
 #include <iostream>
 #include <memory>
 #include <vector>
@@ -16,8 +17,12 @@
 #include "cpr/cpr.h"
 #include "nlohmann/json.hpp"
 #include "result.h"
+#include "uuid.h"
 
 using json = nlohmann::json;
+using std::chrono::duration_cast;
+using std::chrono::seconds;
+using std::chrono::system_clock;
 
 #define LOG_AND_RAISE(err_expr)                                                          \
     do {                                                                                 \
@@ -41,15 +46,24 @@ ABSL_FLAG(std::string, dburl, "https://test.openmprdb.org",
           "database server to connect to");
 ABSL_FLAG(std::string, keyid, "", "ID of the PGP key to use");
 ABSL_FLAG(std::string, server_name, "", "");
+ABSL_FLAG(std::string, player_uuid, "", "");
+ABSL_FLAG(double, points, -99, "");
+ABSL_FLAG(std::string, comment, "", "");
 
 // Required flags:
 // - server_name
 // - keyid
 // - dburl
 ABSL_FLAG(bool, reg, false, "Subcmd: register with remote server.");
-
+// Required flags:
+// - keyid
+// - player_uuid
+// - points
+// - comment
+ABSL_FLAG(bool, submit, false, "Subcmd: submit a new record.");
 ABSL_FLAG(bool, list_servers, false, "Subcmd: list registered servers.");
 
+// Naming convention: method name + API path
 namespace restv1 {
 struct ServerData {
     int64_t id;
@@ -59,7 +73,21 @@ struct ServerData {
     std::string pubkey;
 };
 
-Result<std::string, std::string> ServerRegister(std::string message, std::string pubkey) {
+Result<json, std::string> CheckReply(cpr::Response &rsp) {
+    json rsp_obj = json::parse(rsp.text);
+    if (rsp.status_code >= 200 && rsp.status_code < 300 && rsp_obj["status"] == "OK") {
+        return rsp_obj;
+    }
+    if (rsp_obj["reason"].is_string()) {
+        return Err(rsp_obj["reason"]);
+    } else {
+        return Err("failed to parse reply: " + rsp.text);
+    }
+}
+
+// Returns UUID or error message.
+Result<std::string, std::string> PutServerRegister(std::string message,
+                                                   std::string pubkey) {
     json obj = {{"message", message}, {"public_key", pubkey}};
     std::string payload = obj.dump(2, ' ', true);
     std::cout << payload << std::endl;
@@ -77,28 +105,75 @@ Result<std::string, std::string> ServerRegister(std::string message, std::string
     std::cout << rsp.status_code << std::endl;
     std::cout << rsp.text << std::endl;
 
-    json rsp_obj = json::parse(rsp.text);
-    if (rsp.status_code == 201 && rsp_obj["uuid"].is_string()) {
-        return rsp_obj["uuid"];
-    }
-    if (rsp_obj["reason"].is_string()) {
-        return Err(rsp_obj["reason"]);
-    }
-    return Err("Failed to parse rsp: " + rsp.text);
+    ASSIGN_OR_RAISE(json rsp_obj, CheckReply(rsp));
+    return rsp_obj["uuid"];
 }
 
-Result<std::vector<ServerData>, std::string> ServerList() {
+// Returns UUID or error message.
+Result<std::string, std::string> PutSubmitNew(const std::string &payload) {
+    cpr::Response rsp = cpr::Put(
+        cpr::Url{absl::GetFlag(FLAGS_dburl) + "/v1/submit/new"}, cpr::Body{payload},
+        cpr::Header{{"Content-Type", "text/plain"},
+                    {"User-Agent", "openmprdb-cli-cpp/0.0.0"}},
+        cpr::DebugCallback([](cpr::DebugCallback::InfoType type, std::string data) {
+            if (type == cpr::DebugCallback::InfoType::SSL_DATA_IN ||
+                type == cpr::DebugCallback::InfoType::SSL_DATA_OUT)
+                return;
+            std::cout << data;
+        }));
+    std::cout << rsp.status_code << std::endl;
+    std::cout << rsp.text << std::endl;
 
+    ASSIGN_OR_RAISE(json rsp_obj, CheckReply(rsp));
+    return rsp_obj["uuid"];
+}
+
+// Returns UUID or error message.
+Result<std::string, std::string> DeleteServerUuid(const std::string &server_uuid,
+                                                  const std::string &payload) {
+    cpr::Response rsp = cpr::Delete(
+        cpr::Url{absl::GetFlag(FLAGS_dburl) + "/v1/server/uuid/" + server_uuid},
+        cpr::Body{payload},
+        cpr::Header{{"Content-Type", "text/plain"},
+                    {"User-Agent", "openmprdb-cli-cpp/0.0.0"}},
+        cpr::DebugCallback([](cpr::DebugCallback::InfoType type, std::string data) {
+            if (type == cpr::DebugCallback::InfoType::SSL_DATA_IN ||
+                type == cpr::DebugCallback::InfoType::SSL_DATA_OUT)
+                return;
+            std::cout << data;
+        }));
+    std::cout << rsp.status_code << std::endl;
+    std::cout << rsp.text << std::endl;
+
+    ASSIGN_OR_RAISE(json rsp_obj, CheckReply(rsp));
+    return rsp_obj["uuid"];
+}
+
+// Returns UUID or error message.
+Result<std::string, std::string> DeleteSubmitUuid(const std::string &submit_uuid,
+                                                  const std::string &payload) {
+    cpr::Response rsp = cpr::Delete(
+        cpr::Url{absl::GetFlag(FLAGS_dburl) + "/v1/submit/uuid/" + submit_uuid},
+        cpr::Body{payload},
+        cpr::Header{{"Content-Type", "text/plain"},
+                    {"User-Agent", "openmprdb-cli-cpp/0.0.0"}},
+        cpr::DebugCallback([](cpr::DebugCallback::InfoType type, std::string data) {
+            if (type == cpr::DebugCallback::InfoType::SSL_DATA_IN ||
+                type == cpr::DebugCallback::InfoType::SSL_DATA_OUT)
+                return;
+            std::cout << data;
+        }));
+    std::cout << rsp.status_code << std::endl;
+    std::cout << rsp.text << std::endl;
+
+    ASSIGN_OR_RAISE(json rsp_obj, CheckReply(rsp));
+    return rsp_obj["uuid"];
+}
+
+Result<std::vector<ServerData>, std::string> GetServerList() {
     cpr::Response rsp = cpr::Get(cpr::Url{absl::GetFlag(FLAGS_dburl) + "/v1/server/list"},
                                  cpr::Header{{"User-Agent", "openmprdb-cli-cpp/0.0.0"}});
-
-    json rsp_obj = json::parse(rsp.text);
-    if (rsp.status_code != 200) {
-        if (rsp_obj["reason"].is_string()) {
-            return Err(rsp_obj["reason"]);
-        }
-        return Err("Failed to parse rsp: " + rsp.text);
-    }
+    ASSIGN_OR_RAISE(json rsp_obj, CheckReply(rsp));
 
     std::vector<ServerData> ret;
     for (auto &server : rsp_obj["servers"]) {
@@ -112,6 +187,7 @@ Result<std::vector<ServerData>, std::string> ServerList() {
     }
     return ret;
 }
+
 } // namespace restv1
 
 class KeyManager {
@@ -215,10 +291,37 @@ Result<int, std::string> RegisterMain() {
     ASSIGN_OR_RAISE(std::string pubkey, gpg.ExportPubkey());
     ASSIGN_OR_RAISE(std::string signed_payload, gpg.ClearSign(payload));
 
-    ASSIGN_OR_RAISE(std::string uuid, restv1::ServerRegister(signed_payload, pubkey));
+    ASSIGN_OR_RAISE(std::string uuid, restv1::PutServerRegister(signed_payload, pubkey));
     std::cout << absl::StrFormat("Register name=%s,keyid=%s,dburl=%s,uuid=%s",
                                  server_name, keyid, absl::GetFlag(FLAGS_dburl), uuid)
               << std::endl;
+    return 0;
+}
+
+Result<int, std::string> SubmitMain() {
+    std::string keyid;
+    ASSERT_OR_RAISE((keyid = absl::GetFlag(FLAGS_keyid)) != "");
+    ASSIGN_OR_RAISE(KeyManager gpg, KeyManager::Init(keyid));
+
+    std::string player_uuid = absl::GetFlag(FLAGS_player_uuid);
+    double points = absl::GetFlag(FLAGS_points);
+    ASSERT_OR_RAISE(!player_uuid.empty());
+    ASSERT_OR_RAISE(points >= -1.0 && points <= 1.0);
+
+    const uuids::uuid uuid = uuids::uuid_system_generator{}();
+    long timestamp =
+        duration_cast<seconds>(system_clock::now().time_since_epoch()).count();
+
+    std::string kv_str;
+    absl::StrAppend(&kv_str, "uuid: ", uuids::to_string(uuid), "\n");
+    absl::StrAppend(&kv_str, "timestamp: ", timestamp, "\n");
+    absl::StrAppend(&kv_str, "player_uuid: ", player_uuid, "\n");
+    absl::StrAppend(&kv_str, "points: ", points, "\n");
+    absl::StrAppend(&kv_str, "comment: ", absl::GetFlag(FLAGS_comment));
+    ASSIGN_OR_RAISE(std::string signed_kv, gpg.ClearSign(kv_str));
+    std::cout << signed_kv << std::endl;
+    ASSIGN_OR_RAISE(std::string reply_uuid, restv1::PutSubmitNew(signed_kv));
+    std::cout << "Reply uuid = " << reply_uuid << std::endl;
     return 0;
 }
 
@@ -229,6 +332,8 @@ int main(int argc, char *argv[]) {
     // Checks mutually exclusive args.
     int counter = 0;
     if (absl::GetFlag(FLAGS_reg))
+        counter++;
+    if (absl::GetFlag(FLAGS_submit))
         counter++;
     if (absl::GetFlag(FLAGS_list_servers))
         counter++;
@@ -243,13 +348,17 @@ int main(int argc, char *argv[]) {
 
     if (absl::GetFlag(FLAGS_reg)) {
         RegisterMain().Expect("");
+    } else if (absl::GetFlag(FLAGS_submit)) {
+        SubmitMain().Expect("");
     } else if (absl::GetFlag(FLAGS_list_servers)) {
-        for (const auto &s : restv1::ServerList().Expect("")) {
+        for (const auto &s : restv1::GetServerList().Expect("")) {
             std::cout << absl::StrFormat("%s:\n", s.uuid);
             std::cout << absl::StrFormat("  id:    %d\n", s.id);
             std::cout << absl::StrFormat("  name:  %s\n", s.server_name);
             std::cout << absl::StrFormat("  keyid: %s\n", s.key_id);
         }
+    } else {
+        std::cerr << "unreachable!" << std::endl;
     }
     return 0;
 }
